661e1ac497f22b016c3f7354b6654f9bd43a9572
Gustavo Borges <borgesgustavo360@gmail.com>
2023-06-16 08:42:23 -0400

    Finalizado a tabela de cupons ativos e desativados

--- livraria/resources/js/Pages/ShowCoupons.tsx
+++ livraria/resources/js/Pages/ShowCoupons.tsx
@@ -1,8 +1,46 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import {Head, usePage} from '@inertiajs/react';
 import { PageProps } from '@/types';
+import TableAvailableCupons from "@/Components/TableAvailableCupons";
+import ButtonStatusBarGroup from "@/Components/ButtonStatusBarGroup";
+import Pagination from "@/Components/Pagination";
+import BreadchumbSystem from "@/Components/BreadchumbSystem";
 
-export default function ShowCoupons({ auth }: PageProps) {
+export default function ShowCoupons({ auth }: PageProps, ) {
+
+    let {coupons, statusBar} = usePage().props;
+
+    let header=["Nome", "Valor do desconto", "Status", "Ações", ""];
+    let routes = ["coupon.showActive", "coupon.showInactive"]
+    let title = ["Ativos", "Desativados"]
+
+    let buttonText = () => {
+        if (statusBar === 1) {
+            return ['Ativar']
+        } else {
+            return ['Desativar']
+        }
+    }
+
+    let table = {
+        header: header,
+        data:coupons,
+        actions: buttonText(),
+    }
+
+    const ativosInativos = (): string => {
+        return statusBar === 1
+            ? "ativos"
+            : "desativados"
+    }
+
+    const rotas:object = [
+        {
+            'name': 'Ver Cupons',
+            'route': 'coupon.showActive',
+        }
+    ]
+
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -10,13 +48,18 @@ export default function ShowCoupons({ auth }: PageProps) {
         >
             <Head title="Cupons disponíveis" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">Cupons disponíveis
-                        </div>
-                    </div>
-                </div>
+            <div className="mt-8 ml-20">
+                <BreadchumbSystem rota={rotas}/>
+            </div>
+
+            <ButtonStatusBarGroup routes={routes} status={statusBar} title={title}/>
+
+            {coupons.data.length === 0
+                ? <p className={"text-zinc-600 text-center mt-24 text-3xl font-bold"}>{`Não há cupons ${ativosInativos()}`}</p>
+                : <TableAvailableCupons props={table} statusBar={statusBar}></TableAvailableCupons>}
+
+            <div className={'fixed bottom-0 left-0 right-0 mb-4'}>
+                {coupons.last_page !== 1 ? <Pagination registries={coupons} /> : <></>}
             </div>
         </AuthenticatedLayout>
     );
