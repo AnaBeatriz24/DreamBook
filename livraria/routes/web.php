3fabd5c5688572cb77a49c5f808a1f9d43c95191
Gustavo Borges <borgesgustavo360@gmail.com>
2023-06-18 11:25:03 -0400

    Corrigido erro

--- livraria/routes/web.php
+++ livraria/routes/web.php
@@ -95,6 +95,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/createBook', [BooksController::class, 'create'])->name('book.create');
     Route::get('/showBooks', [BooksController::class, 'show'])->name('book.search');
 
+    Route::get('/showBooks/{gender}', [BooksController::class, 'show'])->name('book.s');
     Route::post('/showBooks/{gender}', [BooksController::class, 'show'])->name('books.searchSubmit');
 
     Route::get('/salesHistory', function () {
